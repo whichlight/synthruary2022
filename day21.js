35a013d0858bb0d1cc50bc909b028d582559c5fd
whichlight <whichlight@gmail.com>
2022-02-21 18:41:51 -0500

    day21

---
+++ day21.js
@@ -0,0 +1,279 @@
+/**
+ * synthruary day 21
+ * Prompt: Microtones
+ * 
+ * <3 whichlight 
+ * 
+ * 
+ */
+
+let contextStarted;
+let w, h;
+const basef = 25; 
+let group;
+let notes = []; 
+let numNotes = 22; 
+let noteMin = 50; 
+let noteMax = 75; 
+const root = noteMin;
+
+
+/*************************
+ * synth start boilerplate 
+ *************************/
+function touchStarted() {
+    if (contextStarted) {
+        synthOn();
+    }
+    return false;
+}
+
+function touchEnded() {
+    if (contextStarted) {
+        synthRelease();
+    }
+
+    if (!contextStarted) {
+        Tone.start();
+        let a = select('#instructions');
+        a.remove();
+        background(0, 0, 0);
+        contextStarted = true;
+    }
+    return false;
+}
+
+function playButton() {
+    push();
+    translate(width * 0.5, height * 0.5);
+    fill(280, 20, 100);
+    noStroke();
+    polygon(0, 0, 50, 3);
+    pop();
+}
+
+function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+        let sx = x + cos(a) * radius;
+        let sy = y + sin(a) * radius;
+        vertex(sx, sy);
+    }
+    endShape(CLOSE);
+}
+
+
+/*************************
+ * p5 main fxns
+ *************************/
+
+function setup() {
+    w = windowWidth;
+    h = windowHeight;
+    colorMode(HSB, 360, 100, 100);
+    createCanvas(w, h);
+    setupSynths();
+    background(0, 0, 0);
+
+    playButton();
+    frameRate(20);
+    noStroke();
+}
+
+function draw() {
+    if (contextStarted) {
+        background(0, 0, 0);
+
+
+        if(mouseIsPressed){
+
+            group.pressed(mouseX,mouseY);
+                
+        }
+
+        //draw notes
+        let notelen = noteMax - noteMin; 
+
+        //x 
+        let notesize = w/notelen; 
+        for(let i = 0; i<notelen; i++){
+            stroke(0,0,100);
+            line(i*notesize, 0,i*notesize, h);
+        }
+
+        //y
+        notesize = h/notelen; 
+        for(let i = 0; i<notelen; i++){
+            stroke(0,0,100);
+            line(0,i*notesize, w,i*notesize);
+        }
+
+    }
+}
+
+/*************************
+ * synthy things 
+ *************************/
+
+ function synthOn() {
+    group.pressed(mouseX, mouseY);
+}
+
+function synthRelease() {
+    group.release();
+}
+
+function setupSynths() {
+   
+    group = new Group();
+    notes = [];
+    let intervals = [];
+    for(let i =0; i<numNotes; i++){
+        intervals.push(i);
+    }
+    for (j = 0; j < 20; j++) {
+        val = intervals.map((i) => (i + numNotes * j));
+        notes = notes.concat(val);
+    }
+}
+
+class Note {
+    constructor(note=root, interval) {
+        this.root = note;
+        this.interval = interval;
+        this.note = note; 
+        this.toplay = [];
+        this.pitch = Tone.Frequency(this.root, "midi");
+        this.filter = new Tone.Filter(800, "lowpass");
+        this.filter.Q.value = 0; 
+        this.osc = new Tone.Synth().connect(this.filter);
+        this.osc.oscillator.type = "sawtooth";
+        this.osc.envelope.decay = 1;
+        this.osc.envelope.sustain = 1;
+        this.osc.envelope.release = 2;
+        this.osc.volume.value = 50;
+        this.pos =createVector(w/2,h/2);
+        this.r = min(100, min(w, h) / 8);
+    }
+
+    isClicked(m) {
+        return (p5.Vector.dist(m, this.pos) < this.r/2)
+    }
+
+    play() {
+        this.osc.triggerAttack(this.pitch, 0.01);
+    }
+
+    setQ(q){
+        this.filter.Q.linearRampToValueAtTime(q,0.5);
+    }
+
+    setVol(v){
+        this.osc.volume.linearRampToValueAtTime(v,0.5);
+
+    }
+
+    setNote(p) {
+        let m = getFrequency((quantize(p)));
+        this.note = p; 
+        this.pitch = m;
+        this.osc.frequency.linearRampToValueAtTime(m,0.5); 
+    }
+
+    release() {
+        this.osc.triggerRelease();
+    }
+
+    update(){
+       
+    }
+
+    display() {
+        
+    }
+}
+
+function getFrequency(_n){
+    let a = 2**(1/12);
+    let n = _n; 
+    let f0 = basef; 
+    let f = f0 * (2**(n/numNotes)); 
+    return f; 
+}
+
+function quantize(p) {
+    p = floor(p);
+    let q = null;
+    let i = 0;
+    while (q == null) {
+        q = notes.find(s => s == p + i);
+        i++;
+    }
+    if (q==-1) q = 0; 
+    return q;
+}
+
+class Group {
+    constructor(synths) {
+        this.synths = [];
+        this.synths.push(new Note(root,0)); 
+        this.synths.push(new Note(root,0)); 
+
+        this.comp = new Tone.Compressor().toDestination();
+        this.comp.threshold.value = -30; 
+        this.comp.ratio.value = 15; 
+        this.comp.release.value = 0.1; 
+        this.comp.knee.value = 10; 
+        this.reverb = new Tone.Reverb(0.5).connect(this.comp);
+
+        this.synths.forEach((s) => { 
+            s.filter.connect(this.reverb);
+        
+        
+        });
+
+    }
+
+    pressed(x, y) {
+        let p = floor(map(mouseX, 0,w,noteMin,noteMax));
+        let q = floor(map(mouseY, 0,h,noteMin,noteMax));
+        let volx = map(mouseX, 0,w,50,30);
+        let voly = map(mouseY,0,h,50,30);
+
+        let u = this.synths[0]; 
+        let v = this.synths[1];
+        
+        u.setNote(p); 
+        v.setNote(q);
+        
+        let m = p%numNotes;
+        let n = q%numNotes; 
+        console.log(m,n, m/n, n/m, p, q);
+
+       // u.setVol(volx);
+        //v.setVol(voly);
+        
+       
+        this.play();
+    }
+
+    play() {
+        this.synths.forEach((s) => { s.play(); });
+    }
+
+    release() {
+        this.synths.forEach((s) => { s.release(); });
+    }
+
+
+}
+
+
+function sum(arr) {
+    return arr.reduce((a, b) => a + b);
+}
+
+
+
+
