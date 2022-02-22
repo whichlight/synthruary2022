6fd81e69cf79bfce92c9d76a59a61c95b445bc88
whichlight <whichlight@gmail.com>
2022-02-21 20:23:38 -0500

    colors

--- day21.js
+++ day21.js
@@ -16,6 +16,7 @@ let numNotes = 22;
 let noteMin = 50; 
 let noteMax = 75; 
 const root = noteMin;
+let xside, yside, notelen; 
 
 
 /*************************
@@ -35,6 +36,8 @@ function touchEnded() {
 
     if (!contextStarted) {
         Tone.start();
+        setupSynths();
+
         let a = select('#instructions');
         a.remove();
         background(0, 0, 0);
@@ -73,17 +76,24 @@ function setup() {
     h = windowHeight;
     colorMode(HSB, 360, 100, 100);
     createCanvas(w, h);
-    setupSynths();
     background(0, 0, 0);
 
     playButton();
     frameRate(20);
     noStroke();
+
+    notelen = noteMax - noteMin; 
+    xside = w/notelen; 
+    yside = h/notelen; 
 }
 
 function draw() {
     if (contextStarted) {
         background(0, 0, 0);
+
+        drawGrid(); 
+        fillGrid();
+ 
 
 
         if(mouseIsPressed){
@@ -92,24 +102,75 @@ function draw() {
                 
         }
 
-        //draw notes
-        let notelen = noteMax - noteMin; 
-
-        //x 
-        let notesize = w/notelen; 
-        for(let i = 0; i<notelen; i++){
-            stroke(0,0,100);
-            line(i*notesize, 0,i*notesize, h);
-        }
-
-        //y
-        notesize = h/notelen; 
-        for(let i = 0; i<notelen; i++){
-            stroke(0,0,100);
-            line(0,i*notesize, w,i*notesize);
-        }
-
+      
     }
+}
+
+function fillGrid(){
+ 
+    for(let p = 0; p<notelen; p++){
+        for(let q = 0; q<notelen; q++){
+            let i = (p)%numNotes; 
+            let j = (q)%numNotes; 
+             let k = min(i,j); 
+             i = i-k; 
+             j = j-k; 
+             let diad = max(i,j);
+             let c = -1; 
+             let s = 100; 
+             let b = 100; 
+             if(diad==0) c = 300; 
+      
+             if(diad==7) c = 120;
+             if(diad==15) c = 160;
+
+
+             if(diad==9) c = 60; 
+             if(diad==13) c = 30;
+
+             if(diad==1){
+                 c=1;
+                 b=40; 
+                 s=50;
+             }
+
+             if(diad==4){
+                 c=120;
+                 s=50;
+                 b=40;
+             }
+             if(diad==18){
+                c=180;
+                s=50;
+                b=40;
+            }
+
+             if(c==-1) {
+                 s = 0;
+                 b = 0; 
+             }
+             fill(c,s,b);
+             rect(p*xside, q*yside, xside, yside);
+        }
+    }
+}
+
+
+function drawGrid(){
+     //draw notes
+     //x 
+     let notesize = w/notelen; 
+     for(let i = 0; i<notelen; i++){
+         stroke(0,0,100);
+         line(i*xside, 0,i*xside, h);
+     }
+
+     //y
+     for(let i = 0; i<notelen; i++){
+         stroke(0,0,100);
+         line(0,i*yside, w,i*yside);
+     }
+
 }
 
 /*************************
@@ -246,15 +307,11 @@ class Group {
         
         u.setNote(p); 
         v.setNote(q);
-        
-        let m = p%numNotes;
-        let n = q%numNotes; 
-        console.log(m,n, m/n, n/m, p, q);
 
-       // u.setVol(volx);
-        //v.setVol(voly);
+        fill(0,0,100);
+        rect((p-noteMin)*xside, (q-noteMin)*yside, xside, yside);
         
-       
+           
         this.play();
     }
 
